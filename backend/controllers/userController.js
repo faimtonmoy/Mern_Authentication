451851b5af96ae3130266ea08af282c950ebd13a
faimtonmoy <bhuiyanfaim@gmail.com>
2023-04-08 20:22:08 +0600

    signup token added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,10 @@
-//login user
+const jwt = require("jsonwebtoken");
 const user = require("../models/userModel");
+
+const createToken = (_id) => {
+  jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
+//login user
 const login = async (req, res) => {
   res.json({ msg: " Log in user" });
 };
@@ -9,8 +14,8 @@ const signup = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
-
-    res.status(200).json({ email, user });
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
