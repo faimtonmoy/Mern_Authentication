64801d4ce7fdbce1293c056ea762424aa5a3fed3
faimtonmoy <bhuiyanfaim@gmail.com>
2023-04-15 23:51:41 +0600

    user log out system added

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,11 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogOut";
 
 const Navbar = () => {
+  const { logout } = useLogout();
+  const handleClick = () => {
+    logout();
+  };
 
   return (
     <header>
@@ -10,13 +15,16 @@ const Navbar = () => {
         </Link>
         <nav>
           <div>
+            <button onClick={handleClick}>Log Out</button>
+          </div>
+          <div>
             <Link to="/login">Log In</Link>
             <Link to="/signup">Sign Up</Link>
           </div>
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
