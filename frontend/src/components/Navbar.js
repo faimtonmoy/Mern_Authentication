f65024a478034d4e903acb95015f2bbeaa5993ea
faimtonmoy <bhuiyanfaim@gmail.com>
2023-04-17 00:28:12 +0600

    initial auth state set

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogOut";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 const Navbar = () => {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
   const handleClick = () => {
     logout();
   };
@@ -14,13 +16,18 @@ const Navbar = () => {
           <h1>Workout Buddy</h1>
         </Link>
         <nav>
-          <div>
-            <button onClick={handleClick}>Log Out</button>
-          </div>
-          <div>
-            <Link to="/login">Log In</Link>
-            <Link to="/signup">Sign Up</Link>
-          </div>
+          {user && (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleClick}>Log Out</button>
+            </div>
+          )}
+          {!user && (
+            <div>
+              <Link to="/login">Log In</Link>
+              <Link to="/signup">Sign Up</Link>
+            </div>
+          )}
         </nav>
       </div>
     </header>
--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
 export const AuthContext = createContext();
 
 export const authReducer = (state, action) => {
@@ -16,10 +16,16 @@ export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
   });
-  console.log("state: ", state);
-  return(
-    <AuthContext.Provider value={{...state, dispatch}}>
-        {children}
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      dispatch({ type: "LOGIN", payload: user });
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
     </AuthContext.Provider>
-  )
+  );
 };
