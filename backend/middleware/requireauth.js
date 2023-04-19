88c1dd375b7d05a126e6e26aa2150c1066999ca2
faimtonmoy <bhuiyanfaim@gmail.com>
2023-04-19 21:11:05 +0600

    making authorized request:backend done

---
+++ backend/middleware/requireauth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+const requireauth = async (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ error: "Authorization token required" });
+  }
+  const token = authorization.split(" ")[1];
+  try {
+    const _id = jwt.verify(token, process.env.Secret);
+    req.user = await User.findOne({ _id }).select("_id");
+    next();
+  } catch (err) {
+    res.status(404).json({
+      error: err,
+    });
+  }
+};
+module.exports= requireauth;
